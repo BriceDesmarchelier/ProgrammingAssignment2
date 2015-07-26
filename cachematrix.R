4cdc7887097989e2852cb583ddc7dbb04d4af8cc
BriceDesmarchelier <desmarchelier.brice@gmail.com>
2015-07-26 22:30:55 +0200

    Assignment 2 released for Rprog Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a special "matrix" that sets, gets both the matrix and its inverse, and cache the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## Calculates the Inverse of a Matrix only if it has not been calculated and cached before
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)  ## replaces the value in the list by the makeCashMatrix
+  m
 }
